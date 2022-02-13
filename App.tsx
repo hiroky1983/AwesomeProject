4815a41506558c900bb51556cd3825b7853e88f8
yamadahiroki <hirockysan1983@gmail.com>
2022-02-13 19:35:24 +0900

    練習

--- App.tsx
+++ App.tsx
@@ -1,10 +1,40 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { Alert, Button, StyleSheet, Text, View } from "react-native";
+
+const teamMember = [
+  {
+    id: 1,
+    name: "ひろっきー",
+  },
+  {
+    id: 2,
+    name: "かたん",
+  },
+  {
+    id: 3,
+    name: "りーべ",
+  },
+  {
+    id: 4,
+    name: "SheepL",
+  },
+];
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text>Hello Qin todos member</Text>
+      {teamMember.map((member) => (
+        <>
+          <Text key={member.id}>{member.name}!</Text>
+          <Button
+            key={member.id}
+            title="押してね"
+            onPress={() => Alert.alert(`Hi ${member.name}`)}
+          />
+        </>
+      ))}
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +43,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
